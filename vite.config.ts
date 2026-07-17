15fe7200236c162d02cc5961fd71b538f581f6d6
Rebecca Newton <rebecca.newton@codecomputerlove.com>
2026-07-17 10:33:48 +0100

    chore: upgrade deps, dev, linting, tests, storybook ok

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,9 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
 	plugins: [reactRouterDevTools(), DevTools(), process.env.NODE_ENV === 'test' ? undefined : reactRouter()],
+	optimizeDeps: {
+		include: ['aria-query', 'lz-string', 'pretty-format']
+	},
 	resolve: {
 		tsconfigPaths: true
 	},
